cc9d6fdc01d3fcfce0b770a4b55d691ef7ad23b9
Kevin Kurek <kevinkurek@live.com>
2018-05-11 15:31:23 -0700

    Plural Exceptions

--- PluralSight.py
+++ PluralSight.py
@@ -1,3 +1,4 @@
+# Break with Loop
 some_list = ['name1','name2','name3','name4']
 
 for i in some_list:
@@ -5,3 +6,37 @@ for i in some_list:
         print('Found name in list')
         break
     print('Test name: {}'.format(i))
+
+
+# Exception Handling
+student_dict = {'name': 'kevin',
+           'student id': 21,
+           'feedback': None}
+
+# print(student['name'])
+
+
+def find(student):
+
+    first_name = student['name']
+
+    try:
+        last_name = student['last_name']
+        # add_number_string = 3 + first_name
+
+    except KeyError:
+        last_name = 'Unknown'
+        print('Error finding last_name')
+
+    except TypeError as error:
+        print('Cant add strings and integers together')
+        print(error) # General Python Output Error
+
+    print(first_name)
+    print(last_name)
+    # print(add_number_string)
+
+    print('Code Ran Fine')
+
+
+find(student_dict)
