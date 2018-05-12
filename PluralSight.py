961571a0a7f0a687b0c0ac8dfb61bdadd13c8039
Kevin Kurek <kevinkurek@live.com>
2018-05-11 23:59:17 -0700

    Classes/Inheritance/Polymorphism

--- PluralSight.py
+++ PluralSight.py
@@ -1,14 +1,21 @@
+#
 # Break with Loop
-some_list = ['name1','name2','name3','name4']
+#
 
-for i in some_list:
-    if i == 'name3':
-        print('Found name in list')
-        break
-    print('Test name: {}'.format(i))
+# some_list = ['name1','name2','name3','name4']
+#
+# for i in some_list:
+#     if i == 'name3':
+#         print('Found name in list')
+#         break
+#     print('Test name: {}'.format(i))
 
 
+
+#
 # Exception Handling
+#
+
 student_dict = {'name': 'kevin',
            'student id': 21,
            'feedback': None}
@@ -39,4 +46,48 @@ def find(student):
     print('Code Ran Fine')
 
 
-find(student_dict)
+#find(student_dict)
+
+
+#
+# Classes, Inheritance, Polymorphism
+#
+
+students = []
+
+# Parent Class
+class Student(object):
+
+    school_name = 'Mark Twain Elementary'
+
+    def __init__(self, name, student_id=332):
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
+
+    def __str__(self):
+        return "Student " + self.name
+
+    def get_name_capitalize(self):
+        return self.name.capitalize()
+
+    def get_school_name(self):
+        return self.school_name
+
+# Child Class of Student
+class HighSchoolStudent(Student):
+
+    school_name = 'Carson High School'
+
+    def get_school_name(self):
+        return 'This is a High School student'
+
+    def get_name_capitalize(self):
+        original_value = super().get_name_capitalize()
+        return original_value + ' -HS'
+
+
+
+james = HighSchoolStudent('james')
+print(james.get_name_capitalize())
+print(james.get_school_name())
